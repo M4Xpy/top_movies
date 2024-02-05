332475c8d6af992a5dd73ac086a565a875af0db5
Maxim V <m4k51m15k4m@gmail.com>
2024-02-05 13:02:00 +0200

    implement home page , add base.html

--- films/views.py
+++ films/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import (
+    Customer,
+    Film,
+    Country,
+    Actor,
+    Genre,
+    Rate,
+    Commentary,
+    Topic,
+)
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_customers = Customer.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_customers": num_customers,
+        "num_visits": num_visits + 1,
+        "num_movies": Film.objects.count(),
+        "num_actors": Actor.objects.count(),
+        "num_countries": Country.objects.count(),
+        "num_genres": Genre.objects.count(),
+        "num_topics": Topic.objects.count(),
+        "num_commentaries": Commentary.objects.count(),
+    }
+
+    return render(request, "films/index.html", context=context)
