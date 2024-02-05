74917a4dc1ab1dd19708729c3764a393ca74fd37
Maxim V <m4k51m15k4m@gmail.com>
2024-02-05 16:32:08 +0200

    add to admin.py CustomerAdmin  and  4 models

--- films/admin.py
+++ films/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Customer, Country, Genre, Actor, Film
+
+
+@admin.register(Customer)
+class CustomerAdmin(UserAdmin):
+    pass
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("name",)
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    search_fields = ("genre",)
+    list_filter = ("genre",)
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    search_fields = ("get_full_name",)
+    list_filter = (
+        "name",
+        "surname",
+    )
+
+
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    list_display = [
+        "film_name",
+        "film_year",
+    ]
+    search_fields = (
+        "film_name",
+        "film_year",
+    )
+    list_filter = ("film_name",)
