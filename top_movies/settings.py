80e9240678ac1225c79088b49168d98e86b811b0
Maxim V <m4k51m15k4m@gmail.com>
2024-02-06 17:22:28 +0200

    save secret_key in env and add env.sample

--- top_movies/settings.py
+++ top_movies/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-6%ee+mbt#e$5wae11618$m4rafqq^@m6-0+_^4twq$32*ro7a-"
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
